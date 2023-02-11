1c579af484f47e53fb9ca677b864f1164feea102
inamprograms <inamul09rehman@gmail.com>
2023-02-11 21:20:10 +0500

    d1000000

---
+++ class04/main.rs
@@ -0,0 +1,89 @@
+// use std::io;
+fn main(){
+//Standard input in rust and type casting
+    // println!("Enter the number: ");
+    // let mut number = String::new();
+
+    // // io::stdin().read_line(&mut number).expect("Io failure");
+    // io::stdin().read_line(&mut number);
+    // // let number:i8 = number.trim().parse().unwrap(); //to type cast
+    // let number:f64 = match number.trim().parse()
+    // {
+    //     Ok(num) => num,
+    //     Err(_) => panic!("Not a number!"),  
+    // };
+    // println!("Number : {}",number);
+
+//Conditional structures : if, if-else, else if
+    // let s1 = 20;
+    // let s2 = 20;
+    // let s3 = 20;
+    // let s4 = 20;
+    // let s5 = 20;
+    // let marks = s1 + s2 + s3 + s4 + s5;
+    // // let marks = (s1 + s2 + s3 + s4 + s5)* 100;
+    // let total = 400;
+    // // let result = (marks /total);
+    // let result = ( marks as f32/total as f32)*100.0;
+    // println!("Result: {}", result);
+
+    // let num = 30;
+    // // let new_num = if num >15 {1} else {8};
+    // let new_num = if num >15 {
+    //     let num:i32 = 14;
+    //     if num == 15{20} else {30}
+    // } else {8};
+    // let mut new_num:i32;
+    // if num >= 15{
+    //     new_num = 10;
+    // }
+    // else{
+    //     new_num = 5;
+    // }
+    // println!("Number: {}, {}", num,new_num);
+
+    // println!("Enter number: ");
+    // let mut number = String::new();
+    // io::stdin().read_line(&mut number);
+    // let number:i8 = number.trim().parse().unwrap();
+    // if number > 40{
+    //     println!("Hi..");
+    // }
+    // else{
+    //     println!("Bye..");
+    // }
+
+//Loops
+    // let mut loop_num:i8 = 0;
+    // loop{
+    //     println!("Value : {}",loop_num);
+    //     loop_num +=1;
+    // }
+
+    // let mut my_num:i32 = 0;
+    // loop{
+    //     let mut my_loop_num:String = String::new();
+    //     println!("Enter value: ");
+    //     io::stdin().read_line(&mut my_loop_num);
+    //         my_num = my_loop_num.trim().parse().unwrap();
+    //         if my_num < 100
+    //           {  break;} else {
+    //               println!("Please enter number less than 100");
+    //           }
+    // }
+    // println!("You entered: {}",my_num);
+
+    // for i in 0..5{ // run on range
+    //     println!("index i {}",i);
+    // }
+    // let arry_number: [i32;3] = [2,4,7];
+    // for number in arry_number{ 
+
+    //     println!("{}",number);
+    // }
+    // let name:&str = "Inam";
+    // for char in name.chars(){
+    //     println!("{}",char);
+    // }
+    
+}
