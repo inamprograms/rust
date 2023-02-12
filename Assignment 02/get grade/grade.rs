fd47465a422664a35d710ed84e6ce0e01bb35f16
inamprograms <inamul09rehman@gmail.com>
2023-02-12 18:14:00 +0500

    Assignment 02 Part 2

---
+++ Assignment 02/get grade/grade.rs
@@ -0,0 +1,14 @@
+
+fn main(){
+    let s1 = 20;
+    let s2 = 20;
+    let s3 = 20;
+    let s4 = 20;
+    let s5 = 20;
+    let marks = s1 + s2 + s3 + s4 + s5;
+    let total = 400;
+    let result = ( marks as f32/total as f32)*100.0;
+    println!("Result: {}", result);
+
+    
+}
