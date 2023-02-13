92e1c01f21cd8968952e61f52663cc761f93f1a2
inamprograms <inamul09rehman@gmail.com>
2023-02-13 12:57:47 +0500

    Assignment 02 Part 2 updated

--- Assignment 02/get grade/grade.rs
+++ Assignment 02/get grade/grade.rs
@@ -1,14 +1,28 @@
-
+use std::io;
 fn main(){
-    let s1 = 20;
-    let s2 = 20;
-    let s3 = 20;
-    let s4 = 20;
-    let s5 = 20;
-    let marks = s1 + s2 + s3 + s4 + s5;
-    let total = 400;
-    let result = ( marks as f32/total as f32)*100.0;
-    println!("Result: {}", result);
+    let mut obtained_marks:f32 = 0.0;
+    println!("Enter your no of subjects: ");
+    let mut no_of_subjects = String::new();
+    io::stdin().read_line(&mut no_of_subjects);
+    let no_of_subjects:i32 = match no_of_subjects.trim().parse(){
+        Ok(num) => num,
+        Err(_) => panic!("Not a number"),
+    };
+    
+    for subject in 0..no_of_subjects {
+        println!("Enter marks of subject {}:",subject);
+        let mut subject_marks = String::new();
+        io::stdin().read_line(&mut subject_marks);
+        let subject_marks:f32 = match subject_marks.trim().parse(){
+            Ok(num) => num,
+            Err(_) => panic!("Not a number 1"),
+        };
+        obtained_marks += subject_marks;
+    }
+
+    let total:f32 = 400.0;
+    let result = ( obtained_marks /total)*100.0;
+    println!("Your result: {}%", result);
 
     
 }
